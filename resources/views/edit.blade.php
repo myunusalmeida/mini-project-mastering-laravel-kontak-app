ca03a4f95a456411354d99044466c26b71de136e
myunusalmeida <muhammadyunusalmeida@gmail.com>
2023-08-09 21:49:00 +0700

    Solving error edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <h4 class="text-dark mb-3">Edit Kontak {{ $item->name }}</h4>
+    <h4 class="text-dark mb-3">Edit Kontak {{ $contact->name }}</h4>
 
-    <form action="{{ route('contacts.update', $item->id) }}" method="post">
+    <form action="{{ route('contacts.update', $contact->id) }}" method="post">
         @csrf
         @method('PUT')
 
         <div class="mb-3">
             <label for="name">Nama Kontak</label>
-            <input type="text" name="name" id="name" value="{{ $item->name }}" class="form-control shadow-none"
+            <input type="text" name="name" id="name" value="{{ $contact->name }}" class="form-control shadow-none"
                 required>
         </div>
         <div class="mb-3">
             <label for="email">Alamat Email</label>
-            <input type="email" name="email" id="email" value="{{ $item->email }}" class="form-control shadow-none"
-                required>
+            <input type="email" name="email" id="email" value="{{ $contact->email }}"
+                class="form-control shadow-none" required>
         </div>
         <div class="mb-3">
             <label for="phone_number">Nomor Telepon</label>
-            <input type="number" name="phone_number" id="phone_number" value="{{ $item->phone_number }}"
+            <input type="number" name="phone_number" id="phone_number" value="{{ $contact->phone_number }}"
                 class="form-control shadow-none" required>
         </div>
         <div class="d-flex gap-2">
